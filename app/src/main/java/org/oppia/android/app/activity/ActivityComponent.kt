3245821b3529f16bbfad9c9fc87ab769b2f4d110
Dev4rd <dev00004dev@gmail.com>
2024-08-21 10:52:38 +0530

    Update ActivityComponent.kt

--- app/src/main/java/org/oppia/android/app/activity/ActivityComponent.kt
+++ app/src/main/java/org/oppia/android/app/activity/ActivityComponent.kt
@@ -4,7 +4,7 @@ import org.oppia.android.app.translation.AppLanguageActivityInjector
 import org.oppia.android.app.utility.datetime.DateTimeUtil
 
 /**
- * Root subcomponent for all activities.
+ * Root subcomponent for all activities AC.
  *
  * Instances of this subcomponent should be created using [ActivityComponentFactory].
  */
